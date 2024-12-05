8e1256cc9d3e72efda4d2310dbaf5b18407b2fc6
Anant Singh <154836519+sanant456@users.noreply.github.com>
2024-12-05 23:26:03 +0530

    Update solution.py

--- Python Codekata/variables and methods/solution.py
+++ Python Codekata/variables and methods/solution.py
@@ -1,1 +1,13 @@
+a = input()
 
+#.... YOUR CODE STARTS HERE ....
+
+def binary_to_hex(binary_str):
+    while len(binary_str)%4!=0:
+        binary_str='0'+binary_str
+    decimal_rep=int(binary_str,2)
+    hex_rep=hex(decimal_rep)[2:]
+    return hex_rep
+print(binary_to_hex(a))
+
+#.... YOUR CODE ENDS HERE ....
