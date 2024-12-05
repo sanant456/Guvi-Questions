bb94e1d814889a43d6798d4fac37066d9201a4d4
Anant Singh <154836519+sanant456@users.noreply.github.com>
2024-12-05 23:19:39 +0530

    Update Solution.py

--- Python Codekata/Classes and Objects/Solution.py
+++ Python Codekata/Classes and Objects/Solution.py
@@ -1,1 +1,29 @@
+class RemainderCalculator:
+  def __init__(self, n, k, a):
+    self.n = n
+    self.k = k
+    self.a = a
 
+def calculate_remainders(self):
+  rem = {}
+  #.... YOUR CODE STARTS HERE ....
+  for num in self.a:
+            remainder = num%self.k
+            if remainder!=0:
+                rem[remainder]=rem.get(remainder,0)+1
+  #.... YOUR CODE ENDS HERE ....
+
+ return rem
+
+def find_maximum_result(self, num):
+  #.... YOUR CODE STARTS HERE ....
+
+  result =0
+        for r, count in rem.items():
+            moves =(self.k * (count - 1)) + (self.k - r)+1
+            result = max(result,moves)
+        return result
+    #.... YOUR CODE ENDS HERE ....
+
+if __name__ = "__main__":
+from sys import stdin, stdout, setrecursionlimit
