629d2e190f0a54a78d62377d26316cff97c346d7
Anant Singh <154836519+sanant456@users.noreply.github.com>
2024-12-05 14:36:30 +0530

    Update Solution.py

--- Python Codekata/Working with an object/Solution.py
+++ Python Codekata/Working with an object/Solution.py
@@ -1,1 +1,26 @@
+class CycleChecker:
+  def __init__(self, f):
+    self.f = f
+    self.n = len(f)
 
+  def check_cycle(self):
+    #.... YOUR CODES STARTS HERE ....
+    for i in range(self.n):
+          a=i
+          b=self.f[i]-1
+          c=self.f[b]-1
+          d=self.f[c]-1
+          if d==a and a!=b and b!=c and c!=d:
+              return "YES"
+      return "NO"
+   #.... YOUR CODES ENDS HERE ....
+if __name__ == "__main__":
+  n = int(input())
+  f = list(map(int, input().split()))
+
+   #.... YOUR CODES STARTS HERE ....
+  cycle_checker=CycleChecker(f)
+    result=cycle_checker.check_cycle()
+    print(result)
+
+   #.... YOUR CODES ENDS HERE ....
