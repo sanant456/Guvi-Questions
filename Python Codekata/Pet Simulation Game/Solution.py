05d98fec5e7825ac30d93db69abaf979265a1810
Anant Singh <154836519+sanant456@users.noreply.github.com>
2024-12-21 20:37:44 +0530

    Update Solution.py

--- Python Codekata/Pet Simulation Game/Solution.py
+++ Python Codekata/Pet Simulation Game/Solution.py
@@ -1,1 +1,49 @@
+class Pet:
+  #.... YOUR CODE STATRS HERE ....
+
+   def eat(self):
+    pass
+   def sleep(self):
+     pass
+   def make_sound(self):
+     pass
+  #.... YOUR CODE ENDS HERE ....
+
+class Cat(Pet):
+  #.... YOUR CODE STARTS HERE ....
+  def eat(self):
+    print("Cat is eating.")
+  def sleep(self):
+    print("Cat is sleeping.")
+  def make_sound(self):
+    print("Cat is purring.")
+
+   #.... YOUR CODE ENDS HERE ....
+class Dog(Pet):
+  #.... YOUR CODE STATRS HERE ....
+    def eat(self):
+      print("Dog is eating.")
+    def sleep(self):
+      print("Dog is sleeping.")
+    def make_sound(self):
+      print("Dog is wagging its tail.")
+    #.... YOUR CODE ENDS HERE ....
+class Bird(Pet):
+     #.... YOUR CODE STARTS HERE ....
+     def eat(self):
+       print("Bird is eating.")
+     def sleep(self):
+       print("Bird is sleeping.")
+     def make_sound(self):
+       print("Bird is chirping.")
+
+   #.... YOUR CODE ENDS HERE ....
+def main():
+
+if __name__ == "__main__":
+  
+  
+
+  
+ 
 
