0d0a5b39193022716b6bf1a3daa179a8142d4b51
Anant Singh <154836519+sanant456@users.noreply.github.com>
2024-12-21 15:55:11 +0530

    Update Solution.py

--- Python Codekata/Encapsulation/Solution.py
+++ Python Codekata/Encapsulation/Solution.py
@@ -1,1 +1,17 @@
+import math
 
+class NumberCalculator:
+    def __init__(self):
+        pass
+
+    def calculate_result(self, n):
+        return n * (n + 1) // 2 - 2 * (2 ** (int(math.log2(n)) + 1) - 1)
+
+if __name__ == "__main__":
+    number_calculator = NumberCalculator()
+    test_cases = int(input())
+
+    for i in range(test_cases):
+        n = int(input())
+        result = number_calculator.calculate_result(n)
+        print(result)
