e9decf145702687099ee392bae801128b2a4eb60
Anant Singh <154836519+sanant456@users.noreply.github.com>
2024-12-21 21:04:11 +0530

    Update Solution.py

--- Python Codekata/Method Overriding/Solution.py
+++ Python Codekata/Method Overriding/Solution.py
@@ -1,1 +1,36 @@
+class Student:
+  #....  YOUR CODE STARTS HERE ....
+  def get_fee(self):
+    pass
 
+  #....  YOUR CODE ENDS HERE ....
+
+ class Undergraduate(Student):
+   def get_fee(self):
+     #.... YOUR CODE STARTS HERE ....
+     return 5000
+
+     #.... YOUR CODE ENDS HERE ....
+
+class Graduate(Student):
+  def __init___(self, credits):
+    #.... YOUR CODE STARTS HERE ....
+    self.credits = credits
+
+
+   #.... YOUR CODE ENDS HERE ....
+
+  def get_fee(self):
+     #.... YOUR CODE STARTS HERE ....
+    return self.credits*50
+
+     #.... YOUR CODE ENDS HERE ....
+credits = int(input())
+
+undergraduate = Undergraduate()
+graduate = Graduate(credits)
+
+programs = [undergraduate, graduate]
+for program in programs:
+  print(program.get_fee())
+    
